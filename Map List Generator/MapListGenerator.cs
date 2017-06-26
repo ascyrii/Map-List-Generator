b4aea4585d9db4dfecb6f5d839bd7fffa75aeb44
vSidezz <sidezzAPB@gmail.com>
2017-06-26 08:41:35 -0400

    Initial Commit
    yay

---
+++ Map List Generator/MapListGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Map_List_Generator
+{
+    public class MapListGenerator
+    {
+        static void Main(string[] args)
+        {
+            string location = AppDomain.CurrentDomain.BaseDirectory;
+            DirectoryInfo d = new DirectoryInfo(location);
+            FileInfo[] files = d.GetFiles("*.bsp");
+
+            if (files.Length < 1) return;
+
+            string output = String.Format(@"{0}mapcycle.txt", location);
+            StreamWriter write = new StreamWriter(output);
+            write.AutoFlush = true;
+
+            foreach(FileInfo file in files)
+            {
+                string temp = file.Name.Replace(".bsp", "");
+                write.WriteLine(temp);
+            }
+
+            Thread.Sleep(10);
+            Console.Beep();
+        }
+    }
+}
